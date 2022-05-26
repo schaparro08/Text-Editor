3c18215950a74b98c40facf634230016a2baa1a4
schaparro08 <101684009+schaparro08@users.noreply.github.com>
2022-05-26 13:43:11 -0400

    update install.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const {warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -27,16 +27,13 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute(({ request }) => request.destination === 'image',
-new CacheFirst({
-  cacheName: 'my-image-cache',
+registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+new StaleWhileRevalidate({
+  cacheName: 'asset-cache',
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxEntries: 60,
-      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-    }),
+    })
+    
   ],
 }));
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,28 +13,44 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+   // Create a connection to the database database and version we want to use.
+   const jateDb = await openDB('jate', 1);
+
+   // Create a new transaction and specify the database and data privileges.
+   const tx = jateDb.transaction('jate', 'readwrite');
+ 
+   // Open up the desired object store.
+   const store = tx.objectStore('jate');
+ 
+
+   const request = store.put({id, content});
+ 
+   // Get confirmation of the request.
+   const result = await request;
+   console.log('success', result);
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  onsole.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  
+  return result.value;
 };
 
 initdb();
